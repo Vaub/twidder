fe3c7f446a07fed46cdf00e6743ffe800f8ea9ad
Vincent <vincent.aube@hotmail.com>
2016-01-21 20:19:01 +0100

    js for view swap

--- app/client.js
+++ app/client.js
@@ -1,8 +1,53 @@
-displayView = function(){
-    // the code required to display a view
-};
-window.onload = function(){
-    //code that is executed as the page is loaded.
-    //You shall put your own custom code here.
-    window.alert("Hello TDDD97!");
-};
+'use strict'
+
+var isElementAView = function(element) {
+	return (element instanceof HTMLElement) && 
+		(element.getAttribute("type") === "text/view");
+}
+
+var displayViewFromId = function(toDisplayId, viewContainerId) {
+	var view = document.getElementById(viewContainerId);
+	var viewToDisplay = document.getElementById(toDisplayId);
+
+	if (!isElementAView(viewToDisplay)) {
+		return false;
+	}
+
+	view.innerHTML = viewToDisplay.innerHTML;
+};
+
+var welcomeView = (function() {
+
+	var isPasswordValid = function(password, repeatPassword) {
+		return (password instanceof HTMLElement && repeat instanceof HTMLElement) &&
+			(password.value) && (password.value === repeat.value);
+	};
+
+	var addEvents = function() {
+		var password = document.getElementById("password");
+		var repeatPassword = document.getElementById("repeat_password");
+
+		repeatPassword.addEventListener("input", function(e) {
+			if (!isPasswordValid(password, repeatPassword)) {
+				alert("password invalid!");
+			}
+		}, false);
+	};
+
+	var view = {
+		displayView: function() {
+			displayViewFromId("welcome_view", "current_view");
+			addEvents();
+		}
+	};
+
+	return view;
+})();
+
+var displayView = function() {
+	welcomeView.displayView();
+};
+
+window.onload = function() {
+	displayView();
+};
