fe7b157448bc5edb6296e071a88bdeeacb6273b3
Vincent <vincent.aube@hotmail.com>
2016-01-23 13:49:41 +0100

    implemented login

--- app/client.js
+++ app/client.js
@@ -1,56 +1,90 @@
 'use strict';
 
-function isElementAView(element) {
-	return (element instanceof HTMLElement) && 
-		(element.getAttribute("type") === "text/view");
-}
+var session;
+var signedInView;
+var welcomeView;
 
-function displayViewFromId(toDisplayId, viewContainerId) {
-	var view = document.getElementById(viewContainerId);
-	var viewToDisplay = document.getElementById(toDisplayId);
+var ViewUtils = {
+    isElementAView: function(element) {
+        return (element instanceof HTMLElement) &&
+            (element.getAttribute("type") === "text/view");
+    },
 
-	if (!isElementAView(viewToDisplay)) {
-		return false;
-	}
+    displayViewFromId: function(toDisplayId, viewContainerId) {
+        var view = document.getElementById(viewContainerId);
+        var viewToDisplay = document.getElementById(toDisplayId);
 
-	view.innerHTML = viewToDisplay.innerHTML;
-}
+        if (!this.isElementAView(viewToDisplay)) {
+            return false;
+        }
 
-function addClass(element, classToAdd) {
-    if (!(element instanceof HTMLElement)) {
-        return false;
+        view.innerHTML = viewToDisplay.innerHTML;
     }
+};
 
-    if (!element.classList.contains(classToAdd)) {
+// Various utilities methods
+var Utils = {
+    // Add a class to an HTMLElement
+    addClass: function(element, classToAdd) {
+        if (!(element instanceof HTMLElement) || element.classList.contains(classToAdd)) {
+            return false;
+        }
         element.classList.add(classToAdd);
-    }
-}
+    },
 
-function removeClass(element, classToRemove) {
-    if (!(element instanceof HTMLElement)) {
-        return false;
-    }
-
-    if (element.classList.contains(classToRemove)) {
+    // Remove a class from an HTMLElement
+    removeClass: function(element, classToRemove) {
+        if (!(element instanceof HTMLElement) || !element.classList.contains(classToRemove)) {
+            return false;
+        }
         element.classList.remove(classToRemove);
     }
-}
+};
 
-var welcomeView = (function() {
+// SignedInView state
+var SignedInView = function(session) {
+
+    return {
+        displayView: function() {
+            ViewUtils.displayViewFromId("login_view", "current_view")
+        }
+    }
+};
+
+// WelcomeView state
+var WelcomeView = function(session) {
 
     var minPasswordLength = 6;
+
+    function isPasswordLengthValid(password) {
+        return (typeof(password) === "string" &&
+                password.length >= minPasswordLength)
+    }
+
+    function validateLoginForm() {
+        var password = document.getElementById("user_password");
+
+        if (password && isPasswordLengthValid(password.value)) {
+            Utils.removeClass(password, "invalid_input");
+        } else {
+            Utils.addClass(password, "invalid_input");
+            return false;
+        }
+
+        return true;
+    }
 
 	function validateSignupForm() {
 		
 		// password
 		var password = document.getElementById("password");
 		var repeatPassword = document.getElementById("repeat_password");
-		if (password.value && password.value.length >= minPasswordLength && (password.value === repeatPassword.value)) {
-			removeClass(password, "invalid_input");
-            removeClass(repeatPassword, "invalid_input");
+		if (password.value && isPasswordLengthValid(password.value) && (password.value === repeatPassword.value)) {
+			Utils.removeClass(password, "invalid_input");
+            Utils.removeClass(repeatPassword, "invalid_input");
 		} else {
-            addClass(password, "invalid_input");
-            addClass(repeatPassword, "invalid_input");
+            Utils.addClass(password, "invalid_input");
+            Utils.addClass(repeatPassword, "invalid_input");
             return false;
 		}
 
@@ -58,13 +92,22 @@ var welcomeView = (function() {
 	}
 
 	function addEvents() {
-		var password = document.getElementById("password");
-		var repeatPassword = document.getElementById("repeat_password");
+        var login = document.getElementById("login");
 		var signup = document.getElementById("signup");
+
+        login.onsubmit = function(e) {
+            if (validateLoginForm()) {
+                var userEmail = document.getElementById("user_email");
+                var userPassword = document.getElementById("user_password");
+                session.signIn(userEmail.value, userPassword.value)
+            }
+
+            return false;
+        };
 
 		signup.onsubmit = function(e) {
 			if (validateSignupForm()) {
-                alert("Everything is valid!")
+                console.log("It's working");
 			}
 
 			return false;
@@ -73,16 +116,61 @@ var welcomeView = (function() {
 
     return {
         displayView: function () {
-            displayViewFromId("welcome_view", "current_view");
+            ViewUtils.displayViewFromId("welcome_view", "current_view");
             addEvents();
         }
     };
-})();
+};
 
+// Session encapsulate the current session state.
+// Upon changes to this state, it'll use the notifySessionChange method
+// ( Currently the displayView() )
+var Session = function(server, notifySessionChange) {
+    var TOKEN = "sessionToken";
+
+    var sessionToken = localStorage.getItem(TOKEN);
+
+    function changeToken(token) {
+        localStorage.setItem(TOKEN, token);
+        sessionToken = token;
+    }
+
+    return {
+        signIn: function(username, password) {
+            var response = server.signIn(username, password);
+            if (response.success) {
+                changeToken(response.data);
+                notifySessionChange();
+            }
+
+            return { success: response.success, message: response.message };
+        },
+
+        isSignedIn: function() {
+            var response = server.getUserMessagesByToken(sessionToken);
+            return response.success;
+        },
+
+        signOut: function() {
+            server.signOut(sessionToken);
+            notifySessionChange();
+        }
+    }
+};
+
+// Main "refresh" of the website
 var displayView = function() {
-	welcomeView.displayView();
+    if (session.isSignedIn()) {
+        signedInView.displayView();
+    } else {
+        welcomeView.displayView();
+    }
 };
 
 window.onload = function() {
+    session = new Session(serverstub, displayView);
+    signedInView = new SignedInView(session);
+    welcomeView = new WelcomeView(session);
+
 	displayView();
 };
