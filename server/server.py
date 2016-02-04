c0de02454185bd7ef436641e252b3e3a7e5e3595
Isabelle <isaccfb@gmail.com>
2016-02-04 13:34:14 +0100

    User object

--- server/server.py
+++ server/server.py
@@ -1,7 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 DATABASE = "database.db"
+
+
+class User(object):
+    def __init__(self, email, password, first_name, family_name, gender, city, country):
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.family_name = family_name
+        self.gender = gender
+        self.city = city
 
 
 @app.route("/")
