0254b318c62b954fbd4e48857918c761e49818e9
Vincent <vincent.aube@hotmail.com>
2016-03-07 15:17:58 +0100

    <Isa> combine chart

--- twidder/static/js/donutchart.js
+++ twidder/static/js/donutchart.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var donutChart;
-var legend;
-
 function DonutChart(element){
+    var donutChart;
+    var legend;
+
     var options = {
         segmentShowStroke: false,
         animateRotate: true,
@@ -13,6 +13,53 @@ function DonutChart(element){
         responsive: true,
         maintainAspectRatio: true
     };
+
+    function convertData(data){
+        var colors = ["#09355C", "#CBCBCB", "#B61B12"]
+        var chartData = [];
+
+        var findName = function(name){
+            return {
+                nb_connected_users:"Number of connected user",
+                nb_posts:"Number of posts",
+                nb_views:"Number of views"
+            }[name] || name
+        };
+
+        Object.keys(data)
+            .filter(function(d) { return data.hasOwnProperty(d) })
+            .forEach( function(d, index){
+
+                var convertedData = {
+                    label: findName(d),
+                    color: colors[index%(colors.length)],
+                    value: data[d]
+                };
+
+                chartData.push(convertedData);
+            });
+
+        return chartData;
+    }
+
+    function updateChart(data){
+        var convertedData = convertData(data);
+        var chartLabels = [];
+
+        donutChart.segments.forEach(function(segment){
+            chartLabels.push(segment.label);
+        });
+
+        convertedData.forEach(function(d, index){
+            if (chartLabels.indexOf(d.label) == (-1)){
+                donutChart.addData(d)
+            } else{
+                donutChart.segments[index].value = d.value;
+            }
+        });
+
+        donutChart.update();
+    }
 
     var context = element.getElementsByClassName("chart_canvas")[0].getContext('2d');
 
@@ -28,49 +75,5 @@ function DonutChart(element){
     }
 }
 
-function updateChart(data){
-    var convertedData = convertData(data);
-    var chartLabels = [];
 
-    donutChart.segments.forEach(function(segment){
-        chartLabels.push(segment.label);
-    });
 
-    convertedData.forEach(function(d, index){
-        if (chartLabels.indexOf(d.label) == (-1)){
-            donutChart.addData(d)
-        } else{
-            donutChart.segments[index].value = d.value;
-        }
-    });
-
-    donutChart.update();
-}
-
-function convertData(data){
-    var colors = ["#09355C", "#CBCBCB", "#B61B12"]
-    var chartData = [];
-
-    var findName = function(name){
-        return {
-            nb_connected_users:"Number of connected user",
-            nb_posts:"Number of posts",
-            nb_views:"Number of views"
-        }[name] || name
-    };
-
-    Object.keys(data)
-        .filter(function(d) { return data.hasOwnProperty(d) })
-        .forEach( function(d, index){
-
-            var convertedData = {
-                label: findName(d),
-                color: colors[index%(colors.length)],
-                value: data[d]
-            };
-
-            chartData.push(convertedData);
-        });
-
-    return chartData;
-}
